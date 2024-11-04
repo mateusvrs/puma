aeca53c4d206725c1fe2952dcda898ad33d51418
mateusvrs <mateusvrs.dev@gmail.com>
2024-11-03 23:15:50 -0300

    feat: cors to api

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,9 +1,12 @@
 import express from 'express'
+import cors from 'cors'
 
 import UsersRouter from './routes/users.js'
 
 const app = express()
 const port = 3000
+
+app.use(cors())
 
 app.use(express.json())
 
