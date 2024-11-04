1890ee6d2cebcb377b011ac06f515cbd0bc8d0a4
mateusvrs <mateusvrs.dev@gmail.com>
2024-11-04 03:13:14 -0300

    fix: set server port for tests

--- backend/scripts/test.sh
+++ backend/scripts/test.sh
@@ -3,21 +3,19 @@ set -e
 
 export DATABASE_PORT=5433
 
-docker compose -f docker-compose.test.yml up db-test -d
+docker compose -f docker-compose.test.yml -p puma-api-test up -d
 
 echo "Waiting for db-test to be healthy..."
 until [ "$(docker inspect -f '{{.State.Health.Status}}' postgres-test)" = "healthy" ]; do
     sleep 0.5
 done
-
 echo 'PostgreSQL is ready'
 
+echo "Running migrations..."
 npx prisma migrate deploy
-
 npx prisma generate
 
-node --experimental-vm-modules --no-warnings node_modules/jest/bin/jest.js
-
-docker compose -f docker-compose.test.yml down db-test -v
+echo "Running tests..."
+PORT=3001 node --experimental-vm-modules --no-warnings node_modules/jest/bin/jest.js --silent
 
 exec "$@"
