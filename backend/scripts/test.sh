fc6a1114f25b115c00ae112a7e666f70689b1610
mateusvrs <mateusvrs.dev@gmail.com>
2024-11-04 04:50:27 -0300

    feat: find available port for backend server

--- backend/scripts/test.sh
+++ backend/scripts/test.sh
@@ -1,21 +1,19 @@
 #!/bin/sh
 set -e
 
-export DATABASE_PORT=5433
+is_port_available() {
+    nc -z localhost "$1" 2>/dev/null
+    return $?
+}
 
-docker compose -f docker-compose.test.yml -p puma-api-test up -d
-
-echo "Waiting for db-test to be healthy..."
-until [ "$(docker inspect -f '{{.State.Health.Status}}' postgres-test)" = "healthy" ]; do
-    sleep 0.5
+for port in $(seq 3001 9892); do
+    if ! is_port_available "$port"; then
+        echo "Running tests on port $port"
+        DATABASE_PORT=5433 PORT="$port" node --experimental-vm-modules --no-warnings node_modules/jest/bin/jest.js --silent
+        exit 0
+    fi
 done
-echo 'PostgreSQL is ready'
 
-echo "Running migrations..."
-npx prisma migrate deploy
-npx prisma generate
-
-echo "Running tests..."
-PORT=3001 node --experimental-vm-modules --no-warnings node_modules/jest/bin/jest.js --silent
+echo "No available port found to run the tests"
 
 exec "$@"
